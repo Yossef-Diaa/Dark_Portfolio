f8aa0c6dc49575890e7bc33b6492cc2edb54c9e1
Yossef-Diaa <yossefdiaa79@gmail.com>
2024-07-07 00:56:46 +0300

    t-5
    5

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
         <div className=" mx-auto flex flex-col-reverse md:flex-row md:px-6 md:space-x-6  items-center">
             <div className="w-1/2 flex flex-col space-y-6 justify-center py-6 text-center md:text-start text-_PrimeDark ">
                 <h1 className='font-bold text-3xl md:text-5xl '>
-                I m Yossef Diaa
+                    I m Yossef Diaa
                 </h1>
                 <p>
-                I m a Front-end web Developer, I love to create beautiful and functional websites. my goal is to learn new thing every day.
+                    I m a Front-end web Developer, I love to create beautiful and functional websites. my goal is to learn new thing every day.
                 </p>
                 <button className="text-purple-950 bg-_PrimeDark py-2 px-6 rounded-full self-center md:self-start hover:bg-purple-300">Download CV</button>
 
